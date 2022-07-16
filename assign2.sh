e17e0f437b2ebc0c47a27c4842cd6f0c04ee2c32
akshaymehta12 <akshaymehta243@gmail.com>
2022-07-16 09:05:08 +0530

    Solution for Assignment5

---
+++ assign2.sh
@@ -0,0 +1,3 @@
+#To get random dice number between 1 t0 6
+
+echo "$(( (RANDOM % 6) + 1 ))" 
---
+++ assign4.sh
@@ -0,0 +1,12 @@
+#Write a program that reads 5 Random 2 Digit values , then find their
+
+sum and the average
+
+
+#!/bin/bash -x
+
+shuf -i 10-99 -n 5
+
+
+
+
